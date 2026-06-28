30ee972dc3c3c4d5ca35eed2600f1165574b0eef
Karina <ka.filipe@gmail.com>
2026-06-28 17:57:21 -0300

    Criei um arquivo de configuração e apliquei variáveis de ambiente

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,6 +1,7 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
+import { pegarBaseURL } from '../utils/variaveis.js';
 
 export const options = {
     stages: [
@@ -16,9 +17,8 @@ export const options = {
 }
 
 export default function () {
-    const url = 'http://localhost:3000/login';
+    const url = pegarBaseURL() + '/login';
 
-    console.log(postLogin)
     const payload = JSON.stringify(postLogin);
 
 
